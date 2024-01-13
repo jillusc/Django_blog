519142586ada049db6a49fe1f344b13eb95d4be3
jillusc <>
2024-01-13 11:25:06 +0000

    Added code for displaying comments

--- blog/models.py
+++ blog/models.py
@@ -38,4 +38,4 @@ class Comment(models.Model):
         ordering = ["created_on"]
 
     def __str__(self):
-        return f"Comment {self.title} by {self.author}"
+        return f"Comment {self.post.title} by {self.author}"
